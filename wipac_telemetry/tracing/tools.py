e866afc006e99c3544dd9ad8cb18a080ae4fdc6a
Ric Evans <emejqz@gmail.com>
2021-04-21 19:55:20 -0500

    events: add `evented()` wrapper, and `add_event()` function

--- wipac_telemetry/tracing/tools.py
+++ wipac_telemetry/tracing/tools.py
@@ -64,3 +64,39 @@ def spanned(
 def get_current_span() -> trace.Span:
     """Get the current span instance."""
     return trace.get_current_span()
+
+
+def evented(
+    event_name: Optional[str] = None, body: Optional[Dict[str, Any]] = None
+) -> Callable[..., Any]:
+    """Decorate to trace a function as a new event.
+
+    The event is added under the current context's span.
+
+    If `event_name` is not provided, the wrapped function's
+    qualified name will be used. If `body` is not provided,
+    the wrapped function's signature and argument values will
+    be used.
+    """
+
+    def inner_function(func: Callable[..., Any]) -> Callable[..., Any]:
+        @wraps(func)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            _event = event_name if event_name else func.__qualname__
+            _body = body if body else _signature_vals(func, args, kwargs)
+
+            logging.getLogger("wipac-telemetry").debug(
+                f"Recorded event `{_event}` for span `{get_current_span()}`"
+            )
+
+            get_current_span().add_event(_event, _body)
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return inner_function
+
+
+def add_event(name: str, body: Dict[str, Any]) -> None:
+    """Add an event to the current span."""
+    get_current_span().add_event(name, body)
