95393d99952099a4bd3b853f9fc625bf3d1b5414
Ric Evans <emejqz@gmail.com>
2021-05-21 16:29:43 -0500

    fix sub-process install

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ import subprocess
 from setuptools import setup  # type: ignore[import]
 
 subprocess.run(
-    "pip install https://github.com/WIPACrepo/wipac-dev-tools.git".split(), check=True
+    "pip install git+https://github.com/WIPACrepo/wipac-dev-tools.git".split(),
+    check=True,
 )
 from wipac_dev_tools import SetupShop  # noqa: E402  # pylint: disable=C0413
 
