11a30dba87586b48f5fe5d5132a3438c482ddcc0
Ric Evans <emejqz@gmail.com>
2021-06-04 15:56:59 -0500

    circleci tweaks

--- examples/wipac_tracing/run_all_examples.sh
+++ examples/wipac_tracing/run_all_examples.sh
@@ -8,8 +8,6 @@ python examples/wipac_tracing/span_client_server_http.py server &
 python examples/wipac_tracing/span_client_server_http.py client &
 wait
 
-docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management &
 python examples/wipac_tracing/span_peer_to_peer_example.py hank george &
 python examples/wipac_tracing/span_peer_to_peer_example.py george hank &
 wait
-docker stop rabbitmq
