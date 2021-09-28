00ea3c8fe4cc83fe3931a2dff3e006f4154d9093
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2021-09-28 12:05:29 -0500

    Type-Hint Backward Compatibility (#33)

--- wipac_telemetry/tracing_tools/spans.py
+++ wipac_telemetry/tracing_tools/spans.py
@@ -5,7 +5,12 @@ import asyncio
 import inspect
 from enum import Enum, auto
 from functools import wraps
-from typing import Any, Callable, Final, List, Optional, TypedDict
+from typing import Any, Callable, List, Optional
+
+try:
+    from typing import Final, TypedDict
+except ImportError:
+    from typing_extensions import Final, TypedDict  # type: ignore[misc]
 
 from opentelemetry.propagate import extract
 from opentelemetry.trace import Span, SpanKind, get_current_span, get_tracer, use_span
