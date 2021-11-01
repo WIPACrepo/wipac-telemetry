83025243015c3f224985123d115cc1f326a07243
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2021-11-01 17:51:23 -0500

    Service Name Patches (#37)

--- wipac_telemetry/tracing_tools/__init__.py
+++ wipac_telemetry/tracing_tools/__init__.py
@@ -1,6 +1,6 @@
 """Init."""
 
-
+import datetime
 import hashlib
 import os
 import sys
@@ -57,16 +57,35 @@ __all__ = [
 # Config SDK ###########################################################################
 
 
+def _pseudo_log(msg: str) -> None:
+    print(f"[wipac-telemetry-setup] {msg}", file=sys.stderr)
+
+
 def get_service_name() -> str:
     """Build the service name from module/script auto-detection."""
-    main_mod_abspath = os.path.abspath(sys.modules["__main__"].__file__)
+    try:
+        main_mod_abspath = os.path.abspath(sys.modules["__main__"].__file__)
+    except AttributeError as e:
+        raise RuntimeError(
+            "WIPAC Telemetry service started up before '__main__' was set. "
+            "Do you have imports in your package's base '__init__.py'? "
+            "If so, remove them; one of these likely prematurely called "
+            "this library before '__main__.py' was executed."
+        ) from e
+    _pseudo_log(f"Detecting Service Name from `{main_mod_abspath}`...")
+
     if main_mod_abspath.endswith("/__main__.py"):
         # this means client is running as a module, so get the full package name + version
         name = main_mod_abspath.rstrip("__main__.py").split("/")[-1]
         here = main_mod_abspath.rstrip(f"/{name}/__main__.py")
-        version = SetupShop._get_version(here, name)  # pylint:disable=protected-access
-        version = ".".join([x.zfill(2) for x in version.split(".")])  # ex: 01.02.03
-        service_name = f"{sys.modules['__main__'].__package__} (v{version})"
+        try:
+            # pylint:disable=protected-access
+            version = SetupShop._get_version(here, name)
+            version = ".".join([x.zfill(2) for x in version.split(".")])  # ex: 01.02.03
+            version = "v" + version
+        except:  # noqa: E722 # pylint:disable=bare-except
+            version = datetime.date.today().isoformat()
+        service_name = f"{sys.modules['__main__'].__package__} ({version})"
     else:
         # otherwise, client is running as a script, so use the file's name
         script = main_mod_abspath.split("/")[-1]  # ex: 'myscript.py'
@@ -74,22 +93,31 @@ def get_service_name() -> str:
             readable_hash = hashlib.sha256(f.read()).hexdigest()
         service_name = f"./{script} ({readable_hash[-4:]})"
 
+    # check if user supplied a prefix
+    if CONFIG["WIPACTEL_SERVICE_NAME_PREFIX"]:
+        _pseudo_log(f"with prefix: \"{CONFIG['WIPACTEL_SERVICE_NAME_PREFIX']}\"")
+        service_name = f"{CONFIG['WIPACTEL_SERVICE_NAME_PREFIX']}/{service_name}"
+
+    _pseudo_log(f'Using Service Name: "{service_name}"')
     return service_name
 
 
+_pseudo_log("Setting Tracer Provider...")
 set_tracer_provider(
     TracerProvider(resource=Resource.create({SERVICE_NAME: get_service_name()}))
 )
 
 
 if CONFIG["WIPACTEL_EXPORT_STDOUT"]:
-    get_tracer_provider().add_span_processor(  # type: ignore[attr-defined]
+    _pseudo_log("Adding ConsoleSpanExporter")
+    get_tracer_provider().add_span_processor(
         # output to stdout
         SimpleSpanProcessor(ConsoleSpanExporter())
     )
 
 if CONFIG["OTEL_EXPORTER_OTLP_ENDPOINT"]:
-    get_tracer_provider().add_span_processor(  # type: ignore[attr-defined]
+    _pseudo_log(f"Adding OTLPSpanExporter ({CONFIG['OTEL_EXPORTER_OTLP_ENDPOINT']})")
+    get_tracer_provider().add_span_processor(
         # relies on env variables
         # -- https://opentelemetry-python.readthedocs.io/en/latest/exporter/otlp/otlp.html
         # OTEL_EXPORTER_OTLP_TRACES_TIMEOUT
@@ -106,3 +134,5 @@ if CONFIG["OTEL_EXPORTER_OTLP_ENDPOINT"]:
         # OTEL_EXPORTER_OTLP_CERTIFICATE
         BatchSpanProcessor(OTLPSpanExporter())
     )
+
+_pseudo_log("Setup complete.")
