e279a7a6f5f04a4d67db5cf4e140822b6999f587
Ric Evans <emejqz@gmail.com>
2022-06-29 16:16:14 -0500

    `set -e`

--- examples/wipac_tracing/run_all_examples.sh
+++ examples/wipac_tracing/run_all_examples.sh
@@ -1,6 +1,8 @@
 # Run all the example python files
 
 set -x
+set -e
+
 export OTEL_EXPORTER_OTLP_ENDPOINT=${OTEL_EXPORTER_OTLP_ENDPOINT:="http://localhost:4318/v1/traces"}
 # export WIPACTEL_EXPORT_STDOUT=${WIPACTEL_EXPORT_STDOUT:="TRUE"}
 
